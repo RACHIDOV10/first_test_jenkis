0c4c43a43d37b2186714a5a3adc324b2252b8fd3
RACHIDOV10 <97804718+RACHIDOV10@users.noreply.github.com>
2025-12-14 21:48:32 +0100

    Update texte dans le contrôleur

--- src/main/java/com/example/Point/of/sale/controller/HelloController.java
+++ src/main/java/com/example/Point/of/sale/controller/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
 public String hello() {
-    return "Hello dear,  from the new Test  :)";
+    return "Hello dear,my name is rachid,  this is the new Test  :)";
 }
 
     @GetMapping("/user")
